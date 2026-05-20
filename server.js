0ce50b72c248d4a1ae0624069f3ba96cee2c1bd0
NothingButTyler <tylerbloxofficialyt@gmail.com>
2026-05-19 20:47:40 -0600

    Implement Discord OAuth callback handling

--- server.js
+++ server.js
@@ -57,13 +57,6 @@ app.get("/auth/discord/callback", async(req,res)=>{
 
         const user = userRes.data;
 
-        // TEMPORARY:
-        // later save to Supabase
-
-res.redirect(
-`https://riglify.github.io/#?username=${encodeURIComponent(user.username)}&avatar=${user.avatar}&id=${user.id}`
-);
-
     }catch(err){
 
         console.log(err.response?.data || err.message);
@@ -83,3 +76,52 @@ localStorage.setItem("riglifyUser", JSON.stringify({
     avatar,
     id
 }));
+
+app.get("/auth/discord/callback", async(req,res)=>{
+
+    const code = req.query.code;
+
+    try{
+
+        const tokenRes = await axios.post(
+            "https://discord.com/api/oauth2/token",
+            new URLSearchParams({
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                grant_type: "authorization_code",
+                code,
+                redirect_uri: REDIRECT_URI
+            }),
+            {
+                headers:{
+                    "Content-Type":"application/x-www-form-urlencoded"
+                }
+            }
+        );
+
+        const accessToken = tokenRes.data.access_token;
+
+        const userRes = await axios.get(
+            "https://discord.com/api/users/@me",
+            {
+                headers:{
+                    Authorization:`Bearer ${accessToken}`
+                }
+            }
+        );
+
+        const user = userRes.data;
+
+        res.redirect(
+            `https://riglify.github.io/?username=${encodeURIComponent(user.username)}&avatar=${user.avatar}&id=${user.id}`
+        );
+
+    }catch(err){
+
+        console.log(err.response?.data || err.message);
+
+        res.send("OAuth failed.");
+
+    }
+
+});
