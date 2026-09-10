c3ede4c6483233192f30d3ab06bab31426411b02
NothingButTyler <tylerbloxofficialyt@gmail.com>
2026-09-09 21:10:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const ROBLOX_API_KEY = process.env.ROBLOX_API_KEY;
-
+console.log(
+    "Roblox API key loaded:",
+    Boolean(ROBLOX_API_KEY),
+    "Length:",
+    ROBLOX_API_KEY?.length
+);
 
 
 /* LOGIN */
